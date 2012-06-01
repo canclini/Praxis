1ac147c17558bbdceb736849a437748886ce6bed
Marcel Canclini <marcel.canclini@gmail.com>
2012-06-01 09:43:45 +0200

    initial commit. rails with bootstrap gem and basich Patient model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Praxis::Application.config.secret_token = '97872b0e2e7df9394e993c00197cb24d6f2661402cf5beed1e3d9a3a356fbf13eb1bfd075ec86dd85ad9abae7ac5eb626fd85990aca25093f585584b468be898'
